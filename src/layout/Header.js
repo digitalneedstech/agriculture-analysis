66d4c21fc9b5665dbcd45979f2857c5df1e32c67
Piyush Bhatia <Piyush_Bhatia@epam.com>
2022-10-06 18:26:39 +0530

    utilities

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,14 +1,18 @@
 import React from "react";
-import {Layout} from "antd";
+import {Button, Layout} from "antd";
 
 const {Header} = Layout;
 
 export default function HeaderComp(props) {
+    const logout=()=>{
+        localStorage.removeItem("token");
+    }
     return <Header className="white-background header">
         <h1 style={
             {
                 padding: 10 + 'px'
             }
         }>Agriculture Project</h1>
+        <Button onClick={()=>logout}></Button>
     </Header>;
 }
