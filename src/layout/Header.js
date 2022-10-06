7cd32de2792597b4621eddfe9c9a74e94b412f45
Piyush Bhatia <Piyush_Bhatia@epam.com>
2022-10-06 18:31:33 +0530

    utilities

--- src/layout/Header.js
+++ src/layout/Header.js
@@ -1,11 +1,14 @@
 import React from "react";
 import {Button, Layout} from "antd";
-
+import { Navigate } from "react-router";
+import { useNavigate } from "react-router-dom";
 const {Header} = Layout;
 
 export default function HeaderComp(props) {
+    const navigate = useNavigate();
     const logout=()=>{
         localStorage.removeItem("token");
+        Navigate("/login");
     }
     return <Header className="white-background header">
         <h1 style={
@@ -13,6 +16,6 @@ export default function HeaderComp(props) {
                 padding: 10 + 'px'
             }
         }>Agriculture Project</h1>
-        <Button onClick={()=>logout}></Button>
+        <Button onClick={()=>logout}>Logout </Button>
     </Header>;
 }
